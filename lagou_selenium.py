4922483c279f882979694fef0dc1955871d82bb0
Mocha-Pudding <zhujiangge6@126.com>
2019-02-14 15:32:20 +0800

    lagou_selenium
    Selenium + Chromedriver来模拟浏览器请求去爬取

--- lagou_selenium.py
+++ lagou_selenium.py
@@ -4,6 +4,9 @@ from selenium import webdriver
 from lxml import etree
 import re
 import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 class LagouSpider(object):
     driver_path = r"G:\ProgramApp\chromedriver\chromedriver.exe"
@@ -15,8 +18,20 @@ class LagouSpider(object):
     # run方法，爬虫的入口
     def run(self):
         self.driver.get(self.url)
-        source = self.driver.page_source
-        self.parse_list_page(source)
+        while True:
+            source = self.driver.page_source
+            WebDriverWait(driver=self.driver,timeout=10).until(
+                EC.presence_of_element_located((By.XPATH,"//div[@class='pager_container']/span[last()]"))
+            )
+            self.parse_list_page(source)
+            next_btn = self.driver.find_element_by_xpath(
+                "//div[@class='pager_container']/span[last()]")  # 让driver模拟鼠标点击“下一页”的行为
+            if "pager_next pager_next_disabled" in next_btn.get_attribute("class"):
+                # 如果不能点击“下一页”，换言之已经到最后一页
+                break
+            else:
+                next_btn.click()
+            time.sleep(1)
 
     # parse_list_page()方法：用来解析列表页的页面
     def parse_list_page(self,source):
@@ -28,9 +43,18 @@ class LagouSpider(object):
 
     # request_detail_page()方法：请求详情页面
     def request_detail_page(self,url):
-        self.driver.get(url)
+        # self.driver.get(url)
+        self.driver.execute_script("window.open('%s')"%url)
+        self.driver.switch_to.window(self.driver.window_handles[1])
+        WebDriverWait(self.driver,timeout=10).until(
+            EC.presence_of_element_located((By.XPATH,"//span[@class='name']"))
+        )
         source = self.driver.page_source
         self.parse_detail_page(source)
+        # 关闭当前这个详情页
+        self.driver.close()
+        # 继续切换回职位列表页
+        self.driver.switch_to.window(self.driver.window_handles[0])
 
     # parse_detail_page()方法：解析详情页面
     def parse_detail_page(self,source):
@@ -45,10 +69,12 @@ class LagouSpider(object):
         education = job_request_spans[3].xpath('.//text()')[0].strip()
         education = re.sub(r"[\s/]", "", education)  # 正则表达式
         desc = "".join(html.xpath("//dd[@class='job_bt']//text()")).strip()
+        company_name = html.xpath("//h2[@class='fl']/text()")[0].strip()
 
         # 保存职位信息到字典里
         position = {
             'name' : position_name,
+            'company_name' : company_name,
             'salary' : salary,
             'city' : city,
             'workyears' : work_years,
